7f25ce476d8ef979505c9089f443a694d9de3281
Andrew Young <ayoung@kabam.com>
2013-07-08 04:40:33 -0700

    Set up model.

---
+++ api/app/controllers/Users.scala
@@ -0,0 +1,34 @@
+package controllers
+
+import play.api.mvc._
+import play.api.libs.json._
+import models._
+import controllers.Actions._
+
+import com.mongodb.casbah.WriteConcern
+import se.radley.plugin.salat._
+import se.radley.plugin.salat.Binders._
+import mongoContext._
+
+object Users extends Controller {
+  
+  def index = Action {
+    val users = User.findAll().toList
+    Ok(Json.toJson(users))
+  }
+
+  def create = JsonAction[User] { user =>
+    User.save(user, WriteConcern.Safe)
+    Ok(Json.toJson(user))
+  }
+
+  def view(username: String) = Action {
+    User.findOneByUsername(username).map { user =>
+      Ok(Json.toJson(user))
+    } getOrElse {
+      NotFound
+    }
+  }
+
+  
+}
--- api/app/models/User.scala
+++ api/app/models/User.scala
@@ -1,9 +1,52 @@
 package models
 
-case class User(id: String, deviceId: String, deviceType: String)
+import play.api.Play.current
+import java.util.Date
+import com.novus.salat._
+import com.novus.salat.annotations._
+import com.novus.salat.dao._
+import com.mongodb.casbah.Imports._
+import se.radley.plugin.salat._
+import mongoContext._
 
-object User {
-  def all(): List[Task] = Nil
-  def create(id: String, deviceId: String, deviceType: String) {}
-  def get(id: String) {}
+case class User(
+  id: ObjectId = new ObjectId,
+  username: String,
+  deviceId: String,
+  deviceType: String
+)
+
+object User extends UserDao with UserJson {
+
+trait UserDao extends ModelCompanion[User, ObjectId] with UserJson {
+  def collection = mongoCollection("users")
+  val dao = new SalatDAO[User, ObjectId](collection) {}
+  def findOneByUsername(username: String): Option[User] = dao.findOne(MongoDBObject("username" -> username))
+
+  // Indexes
+  collection.ensureIndex(DBObject("username" -> 1), "username", unique = true)
+}
+
+/**
+ * Trait used to convert to and from json
+ */
+trait UserJson {
+
+  implicit val userJsonWrite = new Writes[User] {
+    def writes(u: User): JsValue = {
+      Json.obj(
+        "id" -> u.id,
+        "username" -> u.username,
+        "deviceId" -> u.deviceId,
+        "deviceType" -> u.deviceType
+      )
+    }
+  }
+
+  implicit val userJsonRead = (
+    (__ \ 'id).read[ObjectId] ~
+    (__ \ 'username).read[String] ~
+    (__ \ 'deviceId).read[String] ~
+    (__ \ 'deviceType).read[String]
+  )(User.apply _)
 }
